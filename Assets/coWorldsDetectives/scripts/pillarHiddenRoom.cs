0576a9d6faa4646ab517ccf8814ba9382c47e1ab
gabry944 <gabry944@student.liu.se>
2016-10-12 13:41:05 +0200

    start on script, not tested

--- Assets/coWorldsDetectives/scripts/pillarHiddenRoom.cs
+++ Assets/coWorldsDetectives/scripts/pillarHiddenRoom.cs
@@ -2,14 +2,30 @@
 using System.Collections;
 
 public class pillarHiddenRoom : MonoBehaviour {
+    public GameObject hiddenDoor1;
+    public GameObject hiddenDoor2;
+    public bool move;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    Vector3 direction;
+    float distance;
+    // Use this for initialization
+    void Start () {
+        move = false;
+        distance = 0.1f;
+        direction = new Vector3(0.0f, 1.0f, 0.0f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (move)
+        {
+            transform.localPosition = transform.localPosition + (direction * distance);
+        }
+    }
+
+    // Update is called once per frame
+    public void Unlock()
+    {
+        move = true;
+    }
 }

d8695067e5c36d03aa1e779dcbf3adbe1fed3a6c
gabry944 <gabry944@student.liu.se>
2016-10-31 12:32:21 +0100

    bugfixr

--- Assets/coWorldsDetectives/scripts/footstepBehavior.cs
+++ Assets/coWorldsDetectives/scripts/footstepBehavior.cs
@@ -16,27 +16,30 @@ public class footstepBehavior : MonoBehaviour
     private GameObject[] footstepsLeft;
     private GameObject[] footstepsRight;
     private bool playWalk;
-    private int step;
     private int LeftStepIt;
     private int RightStepIt;
     private Vector3 rotaionAxis;
     private List<Vector3> stepsLeft;
     private List<Vector3> stepsRight;
-    private List<float> stepRotation;
-    private float startRot; // rakt fram är negative x
+    private List<Quaternion> stepRotation;
+    private float startRot; // rakt fram är negative z
     private bool left;
 
     void Start()
     {
         playWalk = false;
 
-        startRot = getCurrentAngle(new Vector2(-1, 0), new Vector2(path[1].x- path[0].x, path[1].z- path[0].z));
-        Vector3 startPosLeft = new Vector3(path[0].x, path[0].y + 0.001f, path[0].z - stepWith/2);
-        Vector3 startPosRight = new Vector3(path[0].x, path[0].y + 0.001f, path[0].z + stepWith/2);
+        Vector2 line = new Vector2(path[1].x - path[0].x, path[1].z - path[0].z);
+        startRot = Vector2.Angle(new Vector2(0,-1), line);
+        Debug.Log("StartRot = " + startRot);
+        Vector2 ortLine = PerpendicularClockwise(line);
+
+        Vector3 startPosLeft = new Vector3(path[0].x + ortLine.x * stepWith / 2, path[0].y + 0.001f, path[0].z + ortLine.y *stepWith /2);
+        Vector3 startPosRight = new Vector3(path[0].x - ortLine.x * stepWith / 2, path[0].y + 0.001f, path[0].z - ortLine.y * stepWith /2);
 
         stepsLeft = new List<Vector3>();
         stepsRight = new List<Vector3>();
-        stepRotation = new List<float>();
+        stepRotation = new List<Quaternion>();
 
         footstepsLeft = new GameObject[trackLength];
         footstepsRight = new GameObject[trackLength];
@@ -55,11 +58,14 @@ public class footstepBehavior : MonoBehaviour
             footstepsRight[i] = right;
         }
 
+        Debug.Log("footstepsRight[0].transform.rotation = " + footstepsRight[0].transform.rotation +" "+ footstepsRight[0].transform.rotation.eulerAngles);
+
         leftFoot.SetActive(false);
         rightFoot.SetActive(false);
 
         calculateRoute();
         InvokeRepeating("Walk", 0, 1.0f);
+        PlayWalk();
     }
 
     void Update()
@@ -69,51 +75,42 @@ public class footstepBehavior : MonoBehaviour
 
     private void calculateRoute()
     {
-        //float rotation;
-       /* if (step == 0)
-        {
-            //rotation = startRot;
-            float dist = Vector2.Distance(new Vector2(path[step].x, path[step].z), new Vector2(path[step + 1].x, path[step + 1].z));
-            Vector3 direction = path[step + 1] - path[step];
-            int noOfParts = (int)(dist / stepSize);
-            int part = 0;
-            //while (part < noOfParts - 1)
-            //{
-                Vector3 posLeft = path[step] + (stepSize * part) * direction + new Vector3(0f, 0.001f, -stepWith / 2);
-               // Vector3 posLeft = path[step] + new Vector3(0f, 0.001f, -stepWith / 2);
-                stepsLeft.Add(posLeft);
-                //stepRotation.Add(rotation);
-                part++;
+        int step = 0;
+        Quaternion rotation;
 
-                Vector3 posRight = path[step] + (stepSize * (part)) * direction + new Vector3(0f, 0.001f, stepWith / 2);
-                stepsRight.Add(posRight);
-                //stepRotation.Add(rotation);
-                part++;
-            //}
-            step++;
-        }*/
-
-        while (step < trackLength-1)
+        while (step < path.Length -1)
         {
-            //rotation = getCurrentAngle(new Vector2(path[step].x - path[step-1].x, path[step].z - path[step-1].z), new Vector2(path[step+1].x - path[step].x, path[step+1].z - path[step].z));
+            Vector2 line = new Vector2(path[step + 1].x - path[step].x, path[step + 1].z - path[step].z);
+            Vector2 ortLine = PerpendicularClockwise(line);
+            rotation = getCurrentAngle(line);
             float dist = Vector2.Distance(new Vector2(path[step].x, path[step].z), new Vector2(path[step+1].x, path[step+1].z));
 
             Vector3 direction = path[step + 1] - path[step];
-            //int noOfParts =(int) (dist/stepSize);
+            int noOfParts = (int) (dist/stepSize);
             int part = 0;
-            //while (part < noOfParts-1)
-            //{
-                Vector3 posLeft = path[step]+ (stepSize*part)*direction + new Vector3(0f, 0.001f, -stepWith / 2);
+            
+            Vector3 posLeft = path[step] + new Vector3(ortLine.x * stepWith / 2, 0.001f, ortLine.y * stepWith / 2);
+            stepsLeft.Add(posLeft);
+            stepRotation.Add(rotation);
+            Vector3 posRight = path[step] + new Vector3(-ortLine.x * stepWith / 2, 0.001f, -ortLine.y * stepWith / 2);
+            stepsRight.Add(posRight);
+            stepRotation.Add(rotation);
+            part++;
+
+
+            while (part < noOfParts-1)
+            {
+                posLeft = path[step]+ (stepSize*part)*direction + new Vector3(ortLine.x * stepWith / 2, 0.001f, ortLine.y * stepWith / 2);
                 stepsLeft.Add(posLeft);
-                //stepRotation.Add(rotation);
+                stepRotation.Add(rotation);
                 part++;
 
-                Vector3 posRight = path[step] + (stepSize*(part+1))*direction + new Vector3(0f, 0.001f, stepWith / 2);
+                posRight = path[step] + (stepSize*(part+1))*direction + new Vector3(-ortLine.x * stepWith / 2, 0.001f, -ortLine.y * stepWith / 2);
                 stepsRight.Add(posRight);
-                //stepRotation.Add(rotation);
+                stepRotation.Add(rotation);
                 part++;
-            // }
-            Debug.Log("step " + step + ", stepsLeft.Count " + stepsLeft.Count + ", stepsRight.Count " + stepsRight.Count);
+            }
+            Debug.Log("step " + step + ", noOfParts " + noOfParts + ", stepsLeft.Count " + stepsLeft.Count + ", stepsRight.Count " + stepsRight.Count + ", rotation " + rotation  +" "+ rotation.eulerAngles);
             step++;
         }
     }
@@ -128,12 +125,10 @@ public class footstepBehavior : MonoBehaviour
                     LeftStepIt = 0;
                 if (stepsLeft.Count > 0 )
                 {
-                    Debug.Log(" stepsLeft[0] " + stepsLeft[0]);
                     footstepsLeft[LeftStepIt].transform.position = stepsLeft[0];
-                    //footstepsLeft[LeftStepIt].transform.Rotate(rotaionAxis, stepRotation[0]);
+                    footstepsLeft[LeftStepIt].transform.rotation = stepRotation[0];
                     stepsLeft.RemoveAt(0);
-                    //stepRotation.RemoveAt(0);
-
+                    stepRotation.RemoveAt(0);
                     LeftStepIt++;
                 }
                 left = false;
@@ -146,14 +141,16 @@ public class footstepBehavior : MonoBehaviour
                 if (stepsRight.Count > 0)
                 {
                     footstepsRight[RightStepIt].transform.position = stepsRight[0];
-                    //footstepsRight[RightStepIt].transform.Rotate(rotaionAxis, stepRotation[0]);
+                    footstepsRight[RightStepIt].transform.rotation = stepRotation[0];
                     stepsRight.RemoveAt(0);
-                    //stepRotation.RemoveAt(0);
+                    stepRotation.RemoveAt(0);
                     RightStepIt++;
                 }
                 left = true;
-                Debug.Log(" walk stepsRight.Count = " + stepsRight.Count);
-            }         
+            }
+            Debug.Log(" walk stepsLeft.Count = " + stepsLeft.Count + ", stepsRight.Count = " + stepsRight.Count);
+            if (stepsLeft.Count == 0 && stepsRight.Count == 0)
+                playWalk = false;
         }
     }
 
@@ -161,19 +158,26 @@ public class footstepBehavior : MonoBehaviour
     {
         if (!playWalk)
         {
-            step = 1;
             LeftStepIt = 0;
             RightStepIt = 0;
-            calculateRoute();
             playWalk = true;
             left = true;
             Debug.Log("playwalk");
         }
     }
 
-    public float getCurrentAngle(Vector2 a, Vector2 b)
+    public Quaternion getCurrentAngle(Vector2 a)
     {
-        Debug.Log("Angle(): " + Vector2.Angle(a, b));
-        return Vector2.Angle(a, b);
+        Vector2 startRot = new Vector2(0, -1);
+        float angle = Vector2.Angle(startRot, a);
+        Debug.Log("Angle: " + angle);
+        Quaternion quat = new Quaternion();
+        quat.eulerAngles = new Vector3(0f, angle, 0f);
+        return quat;
+    }
+
+    public Vector2 PerpendicularClockwise(Vector2 v2)
+    {
+        return new Vector2(-v2.y, v2.x);
     }
 }

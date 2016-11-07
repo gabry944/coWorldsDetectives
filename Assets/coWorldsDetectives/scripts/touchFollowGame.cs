05dd0f9b187c55dde8402d8aea37a5a40d4cccff
gabry944 <gabry944@student.liu.se>
2016-11-07 16:31:13 +0100

    puzzle in endroom almost done

--- Assets/coWorldsDetectives/scripts/touchFollowGame.cs
+++ Assets/coWorldsDetectives/scripts/touchFollowGame.cs
@@ -12,18 +12,51 @@ public class touchFollowGame : MonoBehaviour {
     private int number = 0;
     private int current = 0;
     private float currentTime = 0;
+    private bool beforeStart = true;
+    private bool lightOn = true;
     private bool start = false;
     private bool wait = false;
+    private int lookingFor = 0;
+    public bool solved = false;
 
 	// Use this for initialization
 	void Start () {
         CreateGameOrder();
+
+        for(int i = 0; i < lights.Length; i++)
+        {
+            lights[i].intensity = 0;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if(beforeStart)
+        {
+            //plus time
+            currentTime += Time.deltaTime;
+
+            //if current cube should still be lit
+            if (currentTime < lightTime)
+            {
+                if(lightOn)
+                    lights[order[0]].intensity = 2;
+                else
+                    lights[order[0]].intensity = 0;
+            }
+            else
+            {
+                currentTime = 0;
+                lightOn = !lightOn;
+            }
+
+            if (buttons[order[0]].GetComponent<touchListener>().activated)
+            {
+                StartGame();
+            }
+        }
         //show order to touch
-        if (start)
+        else if (start)
         {
             //if not all cubes have been lit
             if (current < number)
@@ -33,12 +66,17 @@ public class touchFollowGame : MonoBehaviour {
 
                 //if current cube should still be lit
                 if (currentTime < lightTime)
-                    lights[current].intensity = 1;
+                {
+                    lights[order[current]].intensity = 1;
+                    if(order[current] == 1)
+                        lights[order[current]].intensity = 2;
+                }
                 else
                 {
-                    lights[current].intensity = 0;
+                    lights[order[current]].intensity = 0;
                     currentTime = 0;
                     current++;
+                    lookingFor = 0;
                 }
             }
             else
@@ -50,26 +88,62 @@ public class touchFollowGame : MonoBehaviour {
         //wait for touch
         else if (wait)
         {
-            if(buttons[order[current]].GetComponent<touchListener>().activated)
+            for (int i = 0; i < buttons.Length; i++)
             {
+                if (buttons[i].GetComponent<touchListener>().activated)
+                {
+                    if (i == order[lookingFor])
+                    {
+                        Debug.Log("Activated " + i);
+                        buttons[i].GetComponent<touchListener>().activated = false;
+                        lookingFor++;
 
+                        if (lookingFor == number)
+                        {
+                            number++;
+                            wait = false;
+                            start = true;
+                            current = 0;
+
+                            if(number > order.Length)
+                            {
+                                start = false;
+                                solved = true;
+                                //get keystone to fall down
+                                keystone.GetComponent<Rigidbody>().useGravity = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("incorrect");
+                        buttons[i].GetComponent<touchListener>().activated = false;
+                        wait = false;
+                        start = true;
+                        current = 0;
+                        number = 1;
+                        break;
+                    }
+                }
             }
         }
-	}
+    }
 
     void CreateGameOrder()
     {
         order = new int[5];
-        order[0] = 2;
-        order[1] = 1;
-        order[2] = 4;
-        order[3] = 2;
-        order[4] = 3;
+        order[0] = 1;
+        order[1] = 0;
+        order[2] = 3;
+        order[3] = 1;
+        order[4] = 2;
     }
 
-    void StartGame()
+    public void StartGame()
     {
         start = true;
+        wait = false;
+        beforeStart = false;
         number = 1;
     }
 }
